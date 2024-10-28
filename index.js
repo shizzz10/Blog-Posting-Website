eafc0dd44cfd9f35fdd8d39e7e3a68cc4a5684b9
Sheraz Khan <skwtanai5@gmail.com>
2024-10-28 22:13:19 +0500

    Update vercel.json to add CSP headers and fixed other bugs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 const port = 3000;
 
 const app = express();
+app.set('view engine', 'ejs');
 const posts = [];
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
